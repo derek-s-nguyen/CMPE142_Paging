f31f439f35ff61cfd02aa225367b2d1345bf490e
tiffanynn <tiffany.phuongn@gmail.com>
2019-04-25 13:02:28 -0700

    nothing much

---
+++ pageTable.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <fstream>
+
+#include "pageTable.hpp"
+
+using namespace std;
---
+++ pageTable.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class pageTable{
+    public:
+    pageTable();
+    void outputPageTable();
+    private:
+    int pageNumber;
+}
