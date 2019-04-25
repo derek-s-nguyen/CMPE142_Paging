9ee119d59c032a2e0a615b8d119b95ec9d4badfa
tiffanynn <tiffany.phuongn@gmail.com>
2019-04-25 14:44:18 -0700

    tried implementing a function to consider status of the process

--- pageTable.cpp
+++ pageTable.cpp
@@ -8,3 +8,14 @@
 #include "pageTable.hpp"
 
 using namespace std;
+
+bool pageTable::checkStatus(char status){
+    if(status == 'C' || status == 'T'){
+        pageStatus = false;
+        pageNumber = 0;
+    }
+    else if(status == 'A' || status == 'R' || status == 'W' || status == 'F'){
+        pageStatus = true;
+    }
+    return pageStatus;
+}
