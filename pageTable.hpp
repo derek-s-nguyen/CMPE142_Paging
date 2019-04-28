f19fb03f2251de7e88a823ff5150e2c98072b726
derek-s-nguyen <48035548+derek-s-nguyen@users.noreply.github.com>
2019-04-28 12:49:47 -0700

    fixing istream/ostream

--- pageTable.hpp
+++ pageTable.hpp
@@ -8,17 +8,17 @@
 using namespace std;
 
 class pageTable{
-    public:
+public:
     pageTable();
     int getPageNumber(); //returns page number
     bool checkStatus(char status); //checks the status for paging in process
     void outputPageTable();
-    istream &operator >>(istream &in_stream, pageTable &page);
-    ostream &operator <<(ostream &out_stream, pageTable &page);
+    friend istream &operator >>(istream &in_stream, pageTable &page);
+    friend ostream &operator <<(ostream &out_stream, pageTable &page);
 
-    private:
+private:
     int processID;
     char processAction;
     int pageNumber; //what page are we on
     bool pageStatus; //checks if the page exists
-}
+};
