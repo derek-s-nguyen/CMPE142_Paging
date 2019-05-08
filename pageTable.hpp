815eb268c1fbb061e63626c7a12bd6b4c0e184fd
tiffanynn <tiffany.phuongn@gmail.com>
2019-05-08 15:28:09 -0700

    only added some bools to determine if page is dirty or not and free pages

--- pageTable.hpp
+++ pageTable.hpp
@@ -11,14 +11,19 @@ class pageTable{
 public:
     pageTable();
     int getPageNumber(); //returns page number
+    int get_processID();
     bool checkStatus(char status); //checks the status for paging in process
+    bool isDirty(); 
+    bool setFree();
     void outputPageTable();
     friend istream &operator >>(istream &in_stream, pageTable &page);
     friend ostream &operator <<(ostream &out_stream, pageTable &page);
 
 private:
-    int processID;
-    char processAction;
+    int processID; //ID of the page
     int pageNumber; //what page are we on
+    bool dirty;
+    bool isAllocated; //bool to check if the page is allocated or not
+    char processAction; //create, allocate, write, read
     bool pageStatus; //checks if the page exists
 };
